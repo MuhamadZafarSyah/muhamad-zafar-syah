05bb0a074e7cb624bbdb16f18b37d2ea5d0ad228
MuhamadZafarSyah <zafarsyah123@gmail.com>
2024-01-22 14:21:36 +0700

    bismillah bisa

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://muhamad-zafar-syah-portfolio.vercel.app'),
 
     'asset_url' => env('ASSET_URL'),
 
